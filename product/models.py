b578899966ebd33c589be2aacd3de0d2d079ad90
Marcio Belloni <prof.belloni@gmail.com>
2024-02-22 14:47:12 -0300

    Commit - model.py alterations (22/02/24)
    Inseri data de criação e de alteração no model.

--- product/models.py
+++ product/models.py
@@ -5,6 +5,8 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100, null=False)
     descricao = models.TextField(null=False)
     valor = models.FloatField(null=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_modificacao = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
